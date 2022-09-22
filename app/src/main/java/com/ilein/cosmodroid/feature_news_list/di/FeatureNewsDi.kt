daf6c0c9090cdb9278f10a717ae0ec9dbcd85eca
Astik <a.zashayev@mail.ru>
2022-09-22 10:21:40 +0300

    инверсия зависимостей для package feature_news_list

--- app/src/main/java/com/ilein/cosmodroid/feature_news_list/di/FeatureNewsDi.kt
+++ app/src/main/java/com/ilein/cosmodroid/feature_news_list/di/FeatureNewsDi.kt
@@ -2,10 +2,10 @@ package com.ilein.cosmodroid.feature_news_list.di
 
 import com.ilein.cosmodroid.feature_news_list.presentation.NewsViewModel
 import com.ilein.cosmodroid.feature_news_list.data.api.RetrofitInstance
-import com.ilein.cosmodroid.feature_news_list.data.Repository.RepositoryImpl
-import com.ilein.cosmodroid.feature_news_list.domain.Interactor.InteractorImpl
-import com.ilein.cosmodroid.feature_news_list.domain.Interactor.Interactor
-import com.ilein.cosmodroid.feature_news_list.domain.Repository.Repository
+import com.ilein.cosmodroid.feature_news_list.data.repository.RepositoryImpl
+import com.ilein.cosmodroid.feature_news_list.domain.interactor.InteractorImpl
+import com.ilein.cosmodroid.feature_news_list.domain.interactor.Interactor
+import com.ilein.cosmodroid.feature_news_list.domain.repository.Repository
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
