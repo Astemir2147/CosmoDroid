0320dbcb779b828e7970748f2c4bb7e9eae3ab4e
Astik <a.zashayev@mail.ru>
2022-11-23 22:31:11 +0300

    Заменил плеер

--- app/src/main/java/com/ilein/cosmodroid/feature_news_list/presentation/news_detail/DetailNewsFragment.kt
+++ app/src/main/java/com/ilein/cosmodroid/feature_news_list/presentation/news_detail/DetailNewsFragment.kt
@@ -1,27 +1,19 @@
 package com.ilein.cosmodroid.feature_news_list.presentation.news_detail
 
+import android.content.Intent
+import android.net.Uri
+import coil.load
 import android.os.Bundle
-import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.media3.common.MediaItem
-import androidx.media3.common.PlaybackException
-import androidx.media3.common.Player
-import androidx.media3.exoplayer.ExoPlayer
 import androidx.navigation.fragment.findNavController
-import at.huber.youtubeExtractor.VideoMeta
-import at.huber.youtubeExtractor.YouTubeExtractor
-import at.huber.youtubeExtractor.YtFile
-import coil.load
 import com.ilein.cosmodroid.ViewBindingFragment
 import com.ilein.cosmodroid.databinding.FragmentDetailNewsBinding
 import com.ilein.cosmodroid.common.modalBottomSheet.presentation.ModalBottomSheet
 import com.ilein.cosmodroid.feature_news_list.presentation.model.NewsItem
 
-class DetailNewsFragment : ViewBindingFragment<FragmentDetailNewsBinding>() {
-    private lateinit var player: ExoPlayer
+class DetailNewsFragment: ViewBindingFragment<FragmentDetailNewsBinding>() {
     private lateinit var newsItem: NewsItem
 
     override val initBinding: (inflater: LayoutInflater, container: ViewGroup?, attachToRoot: Boolean) -> FragmentDetailNewsBinding =
@@ -30,49 +22,12 @@ class DetailNewsFragment : ViewBindingFragment<FragmentDetailNewsBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         withSafeBinding {
-           goBackStack.setOnClickListener { findNavController().popBackStack() }
+            goBackStack.setOnClickListener { findNavController().popBackStack() }
             detailSpinMenu.setOnClickListener { showBottomSheet(newsItem) }
+            imageOfDetailNews.setOnClickListener { openUrlInBrowser(newsItem.videoUrl) }
         }
         newsItem = requireArguments().getSerializable(NEWS_ITEM) as NewsItem
         itemHandler(newsItem)
-        initializePlayer()
-    }
-
-    private fun initializePlayer() {
-        setupPlayer()
-        extractYoutubeVideo()
-
-        nonNullBinding.newsVideo.player = player
-    }
-
-    private fun setupPlayer() {
-        player = ExoPlayer.Builder(requireContext()).build()
-        with(player) {
-            playWhenReady = true
-            seekTo(0, 0)
-            prepare()
-            addListener(object : Player.Listener {
-                override fun onPlayerError(error: PlaybackException) {
-                    nonNullBinding.newsVideo.isVisible = false
-                }
-            })
-        }
-    }
-
-    private fun extractYoutubeVideo() {
-        class PlayerExtractor : YouTubeExtractor(requireContext()) {
-            override fun onExtractionComplete(
-                ytFiles: SparseArray<YtFile>?,
-                videoMeta: VideoMeta?
-            ) {
-                if (ytFiles != null) {
-                    val mediaItem = MediaItem.fromUri(ytFiles[YT_FILES_ITAG].url)
-                    player.setMediaItem(mediaItem)
-
-                }
-            }
-        }
-        PlayerExtractor().extract(newsItem.videoUrl)
     }
 
     private fun showBottomSheet(newsItem: NewsItem) {
@@ -80,9 +35,9 @@ class DetailNewsFragment : ViewBindingFragment<FragmentDetailNewsBinding>() {
         modalBottomSheet.show(parentFragmentManager, ModalBottomSheet.TAG)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        player.release()
+    private fun openUrlInBrowser(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
     }
 
     private fun itemHandler(newsItem: NewsItem) {
@@ -97,6 +52,5 @@ class DetailNewsFragment : ViewBindingFragment<FragmentDetailNewsBinding>() {
 
     companion object {
         private const val NEWS_ITEM = "newsItem"
-        private const val YT_FILES_ITAG = 22
     }
 }
