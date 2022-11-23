5d34b4b65de91c904a0e40b4960aa3debde7344a
Astik <a.zashayev@mail.ru>
2022-11-23 11:22:37 +0300

    Добавил базовый класс ViewBindingFragment для фрагментов отвечаюший за binding и его инициализацию, так же изменения в верстке

--- app/src/main/java/com/ilein/cosmodroid/feature_news_list/presentation/NewsFragment.kt
+++ app/src/main/java/com/ilein/cosmodroid/feature_news_list/presentation/NewsFragment.kt
@@ -3,97 +3,100 @@ package com.ilein.cosmodroid.feature_news_list.presentation
 import com.ilein.cosmodroid.feature_news_list.presentation.adapter.NewsAdapter
 import com.ilein.cosmodroid.databinding.FragmentNewsBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import androidx.fragment.app.Fragment
 import com.ilein.cosmodroid.R
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
+import com.ilein.cosmodroid.ViewBindingFragment
 import com.ilein.cosmodroid.feature_news_list.presentation.model.NewsItem
 import com.ilein.cosmodroid.feature_news_list.presentation.state.NewsViewState
 
-class NewsFragment: Fragment(R.layout.fragment_news) {
-    private lateinit var binding: FragmentNewsBinding
+class NewsFragment: ViewBindingFragment<FragmentNewsBinding>() {
     private lateinit var newsAdapter: NewsAdapter
     private val newsViewModel by viewModel<NewsViewModel>()
 
+    override val initBinding: (LayoutInflater, ViewGroup?, Boolean) -> FragmentNewsBinding =
+        FragmentNewsBinding::inflate
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentNewsBinding.bind(view)
-        with(newsViewModel) {
-            contentState.observe(viewLifecycleOwner, ::handleNewsState)
-            newsViewModel.getNewsList()
-            binding.menuDrop.setText(ALL_TYPE)
-        }
-        newsAdapter = NewsAdapter(
-            showBottomSheet = { showBottomSheet(it) },
-            showDetailNews = { showDetailNews(it) }
-        )
-        binding.newsRecyclerView.adapter = newsAdapter
+        withSafeBinding {
+            with(newsViewModel) {
+                contentState.observe(viewLifecycleOwner, ::handleNewsState)
+                newsViewModel.getNewsList()
+                menuDrop.setText(ALL_TYPE_NAME)
+            }
+            newsAdapter = NewsAdapter(
+                showBottomSheet = ::showBottomSheet,
+                showDetailNews = ::showDetailNews
+            )
+            newsRecyclerView.adapter = newsAdapter
 
-        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, ITEMS)
-        binding.menuDrop.setAdapter(adapter)
-        binding.menuDrop.setOnItemClickListener { _, _, position, _ ->
-            adapter.getItem(position)?.let { menuHandler(it) }
-        }
-    }
-
-    private fun menuHandler(value: String) {
-        when (value) {
-            ALL_TYPE -> newsViewModel.getNewsList()
-            DOCKING -> newsViewModel.getNewsListByType(DOCKING_ID)
-            SPACECRAFT_EVENT -> newsViewModel.getNewsListByType(SPACECRAFT_EVENT_ID)
-            MOON_LANDING -> newsViewModel.getNewsListByType(MOON_LANDING_ID)
+            val adapter =
+                ArrayAdapter(requireContext(), R.layout.list_item, FilterTypes.typeNames())
+            menuDrop.setAdapter(adapter)
+            menuDrop.setOnItemClickListener { _, _, position, _ ->
+                adapter.getItem(position)?.let {
+                    if (it == ALL_TYPE_NAME){
+                        newsViewModel.getNewsList()
+                    } else {
+                        newsViewModel.getNewsListByType(FilterTypes.findTypeByName(it).id)
+                    }
+                }
+            }
         }
     }
 
     private fun handleNewsState(newsState: NewsViewState) {
         layoutHandle(newsState)
         when (newsState) {
-            is NewsViewState.Shimmer -> newsState.shimmer()
+            is NewsViewState.Shimmer -> nonNullBinding.shimmer.startShimmer()
             is NewsViewState.Content -> newsState.content()
             is NewsViewState.Error -> newsState.error()
         }
     }
 
     private fun layoutHandle(state: NewsViewState) {
-        with(binding) {
+        withSafeBinding {
             shimmer.stopShimmer()
             shimmer.isVisible = state is NewsViewState.Shimmer
             typeOfNewsMenu.isVisible = state is NewsViewState.Content
             newsRecyclerView.isVisible = state is NewsViewState.Content
-            errorLayout.isVisible = state is NewsViewState.Error
+            errorLayout.root.isVisible = state is NewsViewState.Error
         }
     }
 
     private fun NewsViewState.Content.content() {
         newsAdapter.setNewsList(list = newsList)
-        binding.newsRecyclerView.adapter = newsAdapter
+        nonNullBinding.newsRecyclerView.adapter = newsAdapter
     }
 
     private fun NewsViewState.Error.error() {
-        with(binding) {
-            btnErrorTryAgain.setOnClickListener { getOnTryAction() }
+        with(nonNullBinding.errorLayout) {
+            btnErrorTryAgain.setOnClickListener { onTryAction() }
             textErrorTitle.setText(error.title)
             textErrorDescription.setText(error.description)
         }
     }
 
-    private fun NewsViewState.Shimmer.shimmer() {
-        binding.shimmer.startShimmer()
-    }
-
-    private fun getOnTryAction() {
-        binding.menuDrop.setText(ALL_TYPE)
-        return newsViewModel.getNewsList()
+    private fun onTryAction() {
+        val currentType = nonNullBinding.menuDrop.text.toString()
+        if (currentType == ALL_TYPE_NAME) {
+            newsViewModel.getNewsList()
+        } else {
+            newsViewModel.getNewsListByType(FilterTypes.findTypeByName(currentType).id)
+        }
     }
 
     private fun showDetailNews(newsItem: NewsItem) {
-        arguments = Bundle().apply {
-            putSerializable(NEWS_ITEM, newsItem)
-        }
-        findNavController().navigate(R.id.action_newsFragment_to_detailNewsFragment, arguments)
+        findNavController().navigate(
+            R.id.action_newsFragment_to_detailNewsFragment,
+            bundleOf(NEWS_ITEM to newsItem)
+        )
     }
 
     private fun showBottomSheet(newsItem: NewsItem) {
@@ -101,15 +104,30 @@ class NewsFragment: Fragment(R.layout.fragment_news) {
         modalBottomSheet.show(parentFragmentManager, ModalBottomSheet.TAG)
     }
 
-    private companion object {
-        private const val ALL_TYPE = "All type"
-        private const val DOCKING = "Docking"
-        private const val DOCKING_ID = 2
-        private const val SPACECRAFT_EVENT = "Spacecraft Event"
-        private const val SPACECRAFT_EVENT_ID = 6
-        private const val MOON_LANDING = "Moon Landing"
-        private const val MOON_LANDING_ID = 7
-        private val ITEMS = listOf(ALL_TYPE, DOCKING, SPACECRAFT_EVENT, MOON_LANDING)
+    enum class FilterTypes(val typeName: String, val id: Int) {
+        ALL(ALL_TYPE_NAME, 1),
+        DOCKING(DOCKING_TYPE_NAME, 2),
+        SPACECRAFT_EVENT(SPACECRAFT_EVENT_TYPE_NAME, 6),
+        MOON_LANDING(MOON_LANDING_TYPE_NAME, 7);
+
+        companion object {
+            fun findTypeByName(typeName: String) = when (typeName) {
+                ALL_TYPE_NAME -> ALL
+                DOCKING_TYPE_NAME -> DOCKING
+                SPACECRAFT_EVENT_TYPE_NAME -> SPACECRAFT_EVENT
+                MOON_LANDING_TYPE_NAME -> MOON_LANDING
+                else -> error("Unable to match $typeName with FilterType")
+            }
+
+            fun typeNames(): List<String> = values().map { it.typeName }
+        }
+    }
+
+    companion object {
+        private const val ALL_TYPE_NAME = "All type"
+        private const val DOCKING_TYPE_NAME = "Docking"
+        private const val SPACECRAFT_EVENT_TYPE_NAME = "Spacecraft Event"
+        private const val MOON_LANDING_TYPE_NAME = "Moon Landing"
         private const val NEWS_ITEM = "newsItem"
     }
 }
