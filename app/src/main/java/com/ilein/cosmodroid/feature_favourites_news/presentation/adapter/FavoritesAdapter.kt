b14a2b2c47f4a4fc1fa6283fbb8fd292048f8044
Astik <a.zashayev@mail.ru>
2022-11-24 18:45:47 +0300

    изменение изоброжений + bag fix

--- app/src/main/java/com/ilein/cosmodroid/feature_favourites_news/presentation/adapter/FavoritesAdapter.kt
+++ app/src/main/java/com/ilein/cosmodroid/feature_favourites_news/presentation/adapter/FavoritesAdapter.kt
@@ -8,40 +8,67 @@ import coil.load
 import com.ilein.cosmodroid.databinding.ItemNewsLayoutBinding
 import com.ilein.cosmodroid.feature_favourites_news.presentation.model.DbNewsItem
 
+interface ItemCallback {
+    fun showBottomSheet(position: Int)
+    fun showDetailNews(position: Int)
+}
+
 class FavoritesAdapter(
     private val showBottomSheet: (newsIntent: DbNewsItem) -> Unit,
     private val showDetailNews: (newsIntent: DbNewsItem) -> Unit
-) :
-    RecyclerView.Adapter<FavoritesAdapter.NewsViewHolder>() {
-    class NewsViewHolder(val itemBinding: ItemNewsLayoutBinding) :
-        RecyclerView.ViewHolder(itemBinding.root)
+) : RecyclerView.Adapter<FavoritesAdapter.NewsViewHolder>(), ItemCallback {
 
-    private var newsList: List<DbNewsItem> = mutableListOf()
+    private val newsList: ArrayList<DbNewsItem> = arrayListOf()
+
+    class NewsViewHolder(
+        private val itemBinding: ItemNewsLayoutBinding,
+        itemCallback: ItemCallback
+    ) : RecyclerView.ViewHolder(itemBinding.root) {
+
+        init {
+            with(itemBinding) {
+                spinMenu.setOnClickListener { itemCallback.showBottomSheet(adapterPosition) }
+                root.setOnClickListener { itemCallback.showDetailNews(adapterPosition) }
+            }
+        }
+
+        fun bind(item: DbNewsItem) {
+            with(itemBinding) {
+                dateOfNews.text = item.date
+                typeOfNews.text = item.type
+                previewOfNews.text = item.description
+                imageOfNews.load(item.featureImage)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder =
         NewsViewHolder(
             ItemNewsLayoutBinding.bind(
                 LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_news_layout, parent, false)
-            )
+                    .inflate(R.layout.item_news_layout, parent, false),
+            ),
+            this
         )
 
     fun setNewsList(list: List<DbNewsItem>) {
-        newsList = list
+        newsList.clear()
+        newsList.addAll(list)
     }
 
     fun getNewsList() = newsList
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        with(holder.itemBinding) {
-            spinMenu.setOnClickListener { showBottomSheet.invoke(newsList[position]) }
-            dateOfNews.text = newsList[position].date
-            typeOfNews.text = newsList[position].type
-            previewOfNews.text = newsList[position].description
-            imageOfNews.load(newsList[position].featureImage)
-            root.setOnClickListener { showDetailNews.invoke(newsList[position]) }
-        }
+        holder.bind(newsList[position])
     }
 
     override fun getItemCount() = newsList.size
+
+    override fun showBottomSheet(position: Int) {
+        showBottomSheet.invoke(newsList[position])
+    }
+
+    override fun showDetailNews(position: Int) {
+        showDetailNews.invoke(newsList[position])
+    }
 }
